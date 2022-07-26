15bf1171cd19f4c26060b2667af258a583e33971
unknown <hquanglai@gmail.com>
2022-07-26 22:33:37 +0700

    first

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/quanglaidb', { useNewUrlParser: true }), BlogModule,],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
